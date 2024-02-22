dcd7797aa17043ab7b47a62c6143570ef64a6691
EdsonJSF <edsonjsf1990@gmail.com>
2024-02-22 10:26:12 -0400

    feat (calendar): react big calendar instalations

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,12 +1,48 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours, format, getDay, parse, startOfWeek } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
 import { Navbar } from "../components";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "title",
+    notes: "notes",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "bgColor",
+    user: {
+      id: "0001",
+      name: "name",
+    },
+  },
+];
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
 
-      <h1>CalendarPage</h1>
-      <hr />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+      />
     </>
   );
 };
